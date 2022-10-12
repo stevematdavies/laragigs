65beed0b4d2b633c731ec62718c6daec5a0ab0fb
stevematdavies <stevematdavies@gmail.com>
2022-10-12 14:11:26 +0300

    First Commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Listing;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\User::factory(10)->create();
+        Listing::factory(6)->create();
+
+    }
+}
